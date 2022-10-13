f5d03a0fcd1d116f2497948b358998d9eb969312
Kinan <kinandiwan@gmail.com>
2022-10-13 15:23:24 +0300

    start python

---
+++ inputs.py
@@ -0,0 +1,15 @@
+
+
+# a = input('Hello Kinan  ')
+# "Hello, - {a}"
+
+
+a = input('your name: ')
+print(a)
+
+b = input('your name: ')
+c = f"Hello {b}"
+print(c)
+
+user_age = input('Age:')
+print(user_age.isdigit())
